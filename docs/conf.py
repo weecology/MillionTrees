0c0ca371c94a93f3491f56f6e18570c9408ac23d
bw4sz <benweinstein2010@gmail.com>
2024-08-08 15:05:39 -0700

    working on apiautodoc on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
+import os
+import sys
+
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, os.path.join(curr_path, '..'))
+sys.path.insert(0, os.path.abspath('..'))
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -16,7 +22,17 @@ master_doc = 'landing'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx.ext.autodoc']
+
+# -- Options for autodoc -----------------------------------------------------
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'special-members': True,
+    'inherited-members': True,
+    'show-inheritance': True,
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
