050ea0b2e8601f9a02bad72fdc69f485feb63091
bw4sz <benweinstein2010@gmail.com>
2025-01-14 17:20:19 -0500

    BCI training and Santos

--- data_prep/BCI.py
+++ data_prep/BCI.py
@@ -3,13 +3,13 @@ from deepforest.preprocess import split_raster
 from deepforest.utilities import read_file
 
 # 2020 manually validated crown map
-gdf = gpd.read_file("BCI_50ha_2020_08_01_crownmap_formatted.shp")
+gdf = gpd.read_file("/orange/ewhite/DeepForest/BCI/BCI_50ha_2020_08_01_crownmap_raw/BCI_50ha_2020_08_01_crownmap_formatted.shp")
 
 # Remove Multipolygons
 gdf = gdf[gdf.geometry.type == "Polygon"]
 gdf["image_path"] = "BCI_50ha_2020_08_01_global.tif"
 gdf["label"] = "Tree"
-annotations = read_file(gdf)
+annotations = read_file(gdf, root_dir="/orange/ewhite/DeepForest/BCI/BCI_50ha_2020_08_01_crownmap_raw/")
 
 rgb = "/orange/ewhite/DeepForest/BCI/BCI_50ha_2020_08_01_crownmap_raw/BCI_50ha_2020_08_01_global.tif"
 # Cut into crops
@@ -22,6 +22,29 @@ crop_annotations = split_raster(
 
 crop_annotations = crop_annotations[["geometry","image_path"]]
 crop_annotations["source"] = "Vasquez et al. 2023"
+
 # Full path
 crop_annotations["image_path"] = crop_annotations["image_path"].apply(lambda x: "/orange/ewhite/DeepForest/BCI/BCI_50ha_2020_08_01_crownmap_raw/crops/"+x)
 crop_annotations.to_csv("/orange/ewhite/DeepForest/BCI/BCI_50ha_2020_08_01_crownmap_raw/annotations.csv")
+
+
+# Training only polygons, inferred from model + validation development, not for testing
+
+gdf_train = gpd.read_file("/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/BCI_50ha_2022_09_29_crownmap_formatted.shp")
+gdf_train = gdf_train[gdf_train.geometry.type == "Polygon"]
+gdf_train["image_path"] = "BCI_50ha_2022_09_29_global.tif"
+gdf_train["label"] = "Tree"
+annotations_train = read_file(gdf_train, root_dir="/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/")
+rgb = "/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/BCI_50ha_2022_09_29_global.tif"
+crop_annotations_train = split_raster(
+    annotations_train,
+    path_to_raster=rgb,
+    patch_size=2000,
+    allow_empty=False,
+    base_dir="/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/crops")
+
+crop_annotations_train = crop_annotations_train[["geometry","image_path"]]
+crop_annotations_train["source"] = "Vasquez et al. 2023 - training"
+crop_annotations_train["image_path"] = crop_annotations_train["image_path"].apply(lambda x: "/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/crops/"+x)
+crop_annotations_train.to_csv("/orange/ewhite/DeepForest/BCI/BCI_50ha_2022_09_29_crownmap_raw/annotations.csv")
+
