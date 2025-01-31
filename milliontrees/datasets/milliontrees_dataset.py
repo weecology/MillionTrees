e1d3857640731e438b8956390dd2f424e48a8d87
bw4sz <benweinstein2010@gmail.com>
2025-01-30 20:15:13 -0500

    docformatter

--- milliontrees/datasets/milliontrees_dataset.py
+++ milliontrees/datasets/milliontrees_dataset.py
@@ -308,7 +308,7 @@ class MillionTreesDataset:
         """Helper function for downloading/updating the dataset if required.
 
         Note that we only do a version check for datasets where the
-        download_url is set. 
+        download_url is set.
         """
         self.check_version()
 
