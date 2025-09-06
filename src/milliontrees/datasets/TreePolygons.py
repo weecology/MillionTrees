c88f11bdc8e12d2c18e3f92ea69fe94956ba77e7
bw4sz <benweinstein2010@gmail.com>
2025-09-06 11:04:01 -0400

    style

--- src/milliontrees/datasets/TreePolygons.py
+++ src/milliontrees/datasets/TreePolygons.py
@@ -20,8 +20,8 @@ from milliontrees.download_unsupervised import run as run_unsupervised
 
 
 class TreePolygonsDataset(MillionTreesDataset):
-    """The TreePolygons dataset is a collection of tree annotations annotated as multi-point polygon
-    locations.
+    """The TreePolygons dataset is a collection of tree annotations annotated
+    as multi-point polygon locations.
 
     The dataset is comprised of many sources from across the world.
 
@@ -65,10 +65,8 @@ class TreePolygonsDataset(MillionTreesDataset):
                 75419767345
         },
         "0.5": {
-            'download_url':
-                "",
-            'compressed_size':
-                ""
+            'download_url': "",
+            'compressed_size': ""
         }
     }
 
@@ -293,7 +291,8 @@ class TreePolygonsDataset(MillionTreesDataset):
         return metadata, x, targets
 
     def create_polygon_mask(self, image_size, vertices):
-        """Create a grayscale image with a white polygonal area on a black background.
+        """Create a grayscale image with a white polygonal area on a black
+        background.
 
         Parameters:
         - image_size (tuple): A tuple representing the dimensions (width, height) of the image.
@@ -319,8 +318,8 @@ class TreePolygonsDataset(MillionTreesDataset):
         return mask_img
 
     def eval(self, y_pred, y_true, metadata):
-        """The main evaluation metric, detection_acc_avg_dom, measures the simple average of the
-        detection accuracies of each domain."""
+        """The main evaluation metric, detection_acc_avg_dom, measures the
+        simple average of the detection accuracies of each domain."""
 
         results = {}
         results_str = ''
